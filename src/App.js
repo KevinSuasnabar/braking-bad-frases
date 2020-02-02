273454b4e45545967dfa253b64452b46ff0581d3
kevinSuasnabar <xkevin_157@hotmail.com>
2020-02-02 12:08:44 -0500

    'proyecto-frases-breaking-bad-terminado'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react';
+import styled from '@emotion/styled';
+
+//importamors el componente Frase
+import Frase from './components/Frase';
+
+const Contenedor = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 5rem;
+    flex-direction: column;
+`;
+
+const Boton = styled.button`
+    background: -webkit-linear-gradient(top left,#007d35 0%,#007d35 40%, #0f574e 100%);
+    background-size: 300px;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #fff;
+    margin-top: 3rem;
+    padding: 1rem 3rem;
+    font-size: 2rem;
+    border:2px solid black;
+`;
 
 function App() {
+
+  //state de frases
+  const [frase, fraseState] = React.useState({});
+
+  //cargar una frase al iniciar la pagina
+  useEffect(()=>{
+    consultarAPI();
+  },[]);
+
+
+  const consultarAPI = async () => {
+    //nos devuelve una promesa
+    const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    //resolvemos la promesa
+    const frase = await api.json();
+
+    //le damos el estado a la frase
+    fraseState(frase[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+
+      <Frase frase={frase}>
+      </Frase>
+      <Boton onClick={() => consultarAPI()}>
+        Obtener Frase
+      </Boton>
+    </Contenedor>
+
   );
 }
 
